7e1b11a2e894d846205274bdebb246f03a1bfb77
Morco <hello.morco@gmail.com>
2024-08-23 14:06:26 +0300

    [#20] Adjust prettier printWidth and reformat code

--- prettier.config.js
+++ prettier.config.js
@@ -6,7 +6,7 @@ export default {
 	embeddedLanguageFormatting: 'auto',
 	endOfLine: 'lf',
 	htmlWhitespaceSensitivity: 'css',
-	printWidth: 120,
+	printWidth: 115, // Approx 120 (tabs are counted as 1, but rendered as 2/3/4)
 	semi: false,
 	singleQuote: true,
 	trailingComma: 'es5',
