c093bbb4213275b2b729bb57f5caca7a61786881
Morco <hello.morco@gmail.com>
2024-08-31 14:30:25 +0300

    [#53] Fix asset paths

--- src-ds/docs/components/docs-token-coding.tsx
+++ src-ds/docs/components/docs-token-coding.tsx
@@ -11,10 +11,10 @@ export const DocsTokenCoding = ({ token, twVars, twSize, cssSize }: Props) => {
 	return (
 		<div className="mr-xs-9 flex gap-xs-3">
 			{twVars.map((twVar: string) => (
-				<DocsTokenCode key={twVar} icon="/tailwind.svg" value={twVar} size={twSize} />
+				<DocsTokenCode key={twVar} icon="./tailwind.svg" value={twVar} size={twSize} />
 			))}
 
-			<DocsTokenCode icon="/css.svg" value={`var(--ds-${token.name})`} size={cssSize} />
+			<DocsTokenCode icon="./css.svg" value={`var(--ds-${token.name})`} size={cssSize} />
 		</div>
 	)
 }
