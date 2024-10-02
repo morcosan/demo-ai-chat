17509cc02566a12465a3b085a0acbaf0eeeb17c9
Morco <hello.morco@gmail.com>
2024-10-02 14:27:07 +0300

    WIP

--- src-app/biz-modules/ai-chat/components/message-item.tsx
+++ src-app/biz-modules/ai-chat/components/message-item.tsx
@@ -14,12 +14,12 @@ export const MessageItem = ({ message, widthClass, secondary }: Props) => {
 	return (
 		<div className="relative flex flex-col items-end">
 			{message.role === 'user' ? (
-				<div className={`mb-xs-5 w-fit rounded-md px-xs-6 py-xs-3 shadow-sm ${userClass}`}>
+				<div className={`mb-xs-9 w-fit rounded-md px-xs-6 py-xs-3 shadow-sm ${userClass}`}>
 					<div className="whitespace-pre-wrap">{message.text}</div>
 				</div>
 			) : (
-				<div className="mb-sm-4 w-full px-xs-5 py-xs-1">
-					<div className="mb-xs-4 flex items-center gap-xs-1">
+				<div className={`${secondary ? 'mb-sm-2' : 'mb-sm-4'} w-full px-xs-5 py-xs-1`}>
+					<div className={`${secondary ? 'mb-xs-2' : 'mb-xs-4'} flex items-center gap-xs-1`}>
 						<div className="flex-center h-sm-0 w-sm-0 rounded-full">
 							<AiChatSvg className="h-xs-8" />
 						</div>
