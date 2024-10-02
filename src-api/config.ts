d051f5981fb6d35bdcec030a2002ac6249cad1d6
Morco <hello.morco@gmail.com>
2024-10-02 18:27:37 +0300

    WIP

--- src-api/config.ts
+++ src-api/config.ts
@@ -1,14 +1,14 @@
-const getCookie = (key: string) => {
+const getCookie = (key: string, fallback: boolean) => {
 	const value = localStorage.getItem(key)
-	return value === null ? true : value === 'true'
+	return value === null ? fallback : value === 'true'
 }
 
 const setCookie = (key: string, value: boolean) => {
 	localStorage.setItem(key, value ? 'true' : 'false')
 }
 
-export const API__RANDOM_ERROR = getCookie('api-random-error')
-export const API__RANDOM_DELAY = getCookie('api-random-delay')
+export const API__RANDOM_ERROR = getCookie('api-random-error', false)
+export const API__RANDOM_DELAY = getCookie('api-random-delay', true)
 
 setCookie('api-random-error', API__RANDOM_ERROR)
 setCookie('api-random-delay', API__RANDOM_DELAY)
