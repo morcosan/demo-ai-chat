422acda748b5076ce8ce6f0f6b259597e6adb693
Morco <hello.morco@gmail.com>
2024-10-03 13:08:19 +0300

    WIP

--- src-api/config.ts
+++ src-api/config.ts
@@ -1,14 +1,28 @@
-const getCookie = (key: string, fallback: boolean) => {
-	const value = localStorage.getItem(key)
-	return value === null ? fallback : value === 'true'
+const computeCookie = <T>(key: string, fallback: T): T => {
+	const valueStr = localStorage.getItem(key)
+	let value = valueStr as T
+
+	if (typeof fallback === 'number') {
+		value = parseFloat(String(valueStr)) as T
+		value = isNaN(value as number) ? fallback : value
+	}
+
+	if (typeof fallback === 'boolean') {
+		value = valueStr === null ? fallback : ((valueStr === 'true') as T)
+	}
+
+	value && setCookie(key, value)
+
+	return value
 }
 
-const setCookie = (key: string, value: boolean) => {
-	localStorage.setItem(key, value ? 'true' : 'false')
+const setCookie = (key: string, value: unknown) => {
+	if (typeof value === 'boolean') {
+		localStorage.setItem(key, value ? 'true' : 'false')
+	} else {
+		localStorage.setItem(key, String(value))
+	}
 }
 
-export const API__RANDOM_ERROR = getCookie('api-random-error', false)
-export const API__RANDOM_DELAY = getCookie('api-random-delay', true)
-
-setCookie('api-random-error', API__RANDOM_ERROR)
-setCookie('api-random-delay', API__RANDOM_DELAY)
+export const API__RANDOM_ERROR = computeCookie('api-random-error', false)
+export const API__MAX_THROTTLING = computeCookie('api-max-throttling', 2000)
--- src-api/utilities/network.ts
+++ src-api/utilities/network.ts
@@ -1,4 +1,4 @@
-import { API__RANDOM_DELAY, API__RANDOM_ERROR } from '@api/config'
+import { API__MAX_THROTTLING, API__RANDOM_ERROR } from '@api/config'
 import { randomFalse, randomInt } from '@utils/release'
 import { ApiResponse, STATUS__NOT_FOUND, STATUS__SERVER_ERROR } from '../types'
 
@@ -6,10 +6,8 @@ export const RESP__NOT_FOUND: ApiResponse = { status: STATUS__NOT_FOUND, data: n
 export const RESP__SERVER_ERROR: ApiResponse = { status: STATUS__SERVER_ERROR, data: null }
 
 export const applyNetwork = async (resp: ApiResponse | Promise<ApiResponse>): Promise<ApiResponse> => {
-	if (API__RANDOM_DELAY) {
-		// Simulate network throttling
-		await wait(randomInt(0, 3000))
-	}
+	// Simulate network throttling
+	await wait(randomInt(0, API__MAX_THROTTLING))
 
 	// Simulate server crash
 	return API__RANDOM_ERROR && randomFalse() ? RESP__SERVER_ERROR : resp
