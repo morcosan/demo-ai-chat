faa22885d0719ab94f376e1a71147af45d622eb8
Morco <hello.morco@gmail.com>
2024-10-08 10:37:32 +0300

    WIP

--- src-app/layouts/app-layout.tsx
+++ src-app/layouts/app-layout.tsx
@@ -1,7 +1,7 @@
 import { SettingsMenu } from '@app/layouts/navbar/_settings-menu'
 import { useUiViewport } from '@ds/release'
 import { useEffect, useState } from 'react'
-import { NavTab } from './navbar/_types'
+import { useLocation } from 'react-router-dom'
 import { DesktopNavbar } from './navbar/desktop-navbar'
 import { MobileNavMenu } from './navbar/mobile-nav-menu'
 import { MobileNavbar } from './navbar/mobile-navbar'
@@ -11,15 +11,21 @@ interface Props extends ReactProps {
 }
 
 export const AppLayout = ({ pageClassName = '', children }: Props) => {
-	const [navTab, setNavTab] = useState<NavTab>(NavTab.MODULE)
+	const [hasMenu, setHasMenu] = useState(false)
+	const [hasSettings, setHasSettings] = useState(false)
 	const { isViewportMaxLG } = useUiViewport()
+	const location = useLocation()
 
-	const onToggleNavMenu = () => setNavTab(navTab !== NavTab.MODULE ? NavTab.MODULE : NavTab.MENU)
-	const onToggleSettings = () => setNavTab(navTab === NavTab.SETTINGS ? NavTab.MENU : NavTab.SETTINGS)
+	const onToggleNavMenu = () => setHasMenu(!hasMenu)
+	const onToggleSettings = () => setHasSettings(!hasSettings)
 
 	useEffect(() => {
-		setNavTab(NavTab.MODULE)
+		setHasMenu(false)
 	}, [isViewportMaxLG])
+
+	useEffect(() => {
+		setHasMenu(false)
+	}, [location.pathname])
 
 	return (
 		<div
@@ -28,17 +34,23 @@ export const AppLayout = ({ pageClassName = '', children }: Props) => {
 		>
 			{isViewportMaxLG ? <MobileNavbar onToggleNavMenu={onToggleNavMenu} /> : <DesktopNavbar />}
 
-			{navTab === NavTab.MODULE ? (
-				<div className={`h-full w-full flex-1 ${pageClassName}`}>{children}</div>
-			) : (
-				<div className="h-full w-full flex-1">
-					{navTab === NavTab.SETTINGS ? (
-						<SettingsMenu onClickBack={onToggleSettings} />
-					) : (
-						<MobileNavMenu onToggleSettings={onToggleSettings} />
-					)}
-				</div>
-			)}
+			<div
+				className={[
+					'absolute bottom-0 left-0 z-popup w-full',
+					'border-t border-color-border-shadow shadow-lg',
+					'transition-transform duration-300 ease-out',
+					hasMenu ? 'translate-x-0' : '-translate-x-full',
+				].join(' ')}
+				style={{ top: 'var(--app-spacing-navbar-h)', background: 'var(--app-color-bg-navbar)' }}
+			>
+				{hasSettings ? (
+					<SettingsMenu onClickBack={onToggleSettings} />
+				) : (
+					<MobileNavMenu onToggleSettings={onToggleSettings} />
+				)}
+			</div>
+
+			<div className={`h-full w-full flex-1 ${pageClassName}`}>{children}</div>
 		</div>
 	)
 }
--- src-app/layouts/navbar/_types.ts
+++
@@ -1,5 +0,0 @@
-export enum NavTab {
-	MODULE,
-	MENU,
-	SETTINGS,
-}
