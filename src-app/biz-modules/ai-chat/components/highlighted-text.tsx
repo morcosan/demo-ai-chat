a06a81b5a3bd0a74203e72360eb5711a3740ecfc
Morco <hello.morco@gmail.com>
2024-10-17 21:39:24 +0300

    WIP

--- src-app/biz-modules/ai-chat/components/highlighted-text.tsx
+++ src-app/biz-modules/ai-chat/components/highlighted-text.tsx
@@ -1,20 +1,24 @@
-interface Props {
+interface Props extends ReactProps {
 	text: string
 	keyword: string
 }
 
-export const HighlightedText = ({ text, keyword }: Props) => {
+export const HighlightedText = ({ text, keyword, className }: Props) => {
 	const lcKeyword = keyword.toLowerCase()
 	const line = text.split('\n').find((line) => line.toLowerCase().includes(lcKeyword)) || ''
 	const parts = line.split(new RegExp(`(${keyword})`, 'gi'))
 
-	return parts.map((part, j) =>
-		part.toLowerCase() === lcKeyword ? (
-			<span key={j} className="bg-color-yellow-2 text-color-yellow-4">
-				{part}
-			</span>
-		) : (
-			part
-		)
+	return (
+		<span className={className}>
+			{parts.map((part, j) =>
+				part.toLowerCase() === lcKeyword ? (
+					<span key={j} className="bg-color-yellow-2 text-color-yellow-4">
+						{part}
+					</span>
+				) : (
+					part
+				)
+			)}
+		</span>
 	)
 }
--- src-app/biz-modules/ai-chat/components/search-result-item.tsx
+++ src-app/biz-modules/ai-chat/components/search-result-item.tsx
@@ -1,4 +1,5 @@
-import { Button, SplitSvg } from '@ds/release'
+import { useSettings } from '@app/biz-modules/user-settings/state'
+import { AiChatSvg, Button, SplitSvg } from '@ds/release'
 import { SearchResult } from '../api'
 import { HighlightedText } from './highlighted-text'
 
@@ -9,14 +10,8 @@ interface Props {
 }
 
 export const SearchResultItem = ({ result, keyword, onClick }: Props) => {
+	const { avatar, name } = useSettings()
 	const isSubchat = result.parentId !== result.chatId
-
-	const messageClass = cx({
-		'mx-button-px-item mt-xs-1 rounded-sm px-xs-3 py-xs-2 text-size-sm': true,
-		'bg-color-secondary-bg text-color-secondary-text-default': result.role === 'user' && isSubchat,
-		'bg-color-primary-bg text-color-primary-text-default': result.role === 'user' && !isSubchat,
-		'bg-color-bg-preview text-color-text-subtle': result.role === 'agent',
-	})
 
 	return (
 		<li className="flex flex-col">
@@ -26,25 +21,40 @@ export const SearchResultItem = ({ result, keyword, onClick }: Props) => {
 				tooltip={t('aiChat.action.openChat')}
 				onClick={onClick}
 			>
-				<span className="flex w-full items-center gap-xs-1">
+				<span className="flex w-full items-center gap-xs-2">
+					{/* SUBCHAT ICON */}
 					{Boolean(isSubchat) && <SplitSvg className="h-xs-9 min-w-xs-9 text-color-secondary-text-default" />}
 
+					{/* CHAT TITLE */}
 					<span className={cx('flex-1 truncate', isSubchat && 'text-color-secondary-text-default')}>
 						{result.chat.title}
 					</span>
 
+					{/* MESSAGE COUNT */}
 					<span className="ml-xs-3 hidden text-size-xs text-color-text-subtle sm:block">
 						{t('aiChat.xMessages', { count: result.chat.size })}
 					</span>
 				</span>
 			</Button>
 
+			{/* MESSAGE COUNT - MOBILE */}
 			<div className="mb-xs-2 px-button-px-item text-size-xs text-color-text-subtle sm:hidden">
 				{t('aiChat.xMessages', { count: result.chat.size })}
 			</div>
 
-			<div className={messageClass}>
-				<HighlightedText text={result.text} keyword={keyword} />
+			{/* AGENT NAME */}
+			<div className="mt-xs-0 flex items-center gap-xs-2 px-button-px-item text-size-xs text-color-text-subtle">
+				{result.role === 'agent' ? (
+					<AiChatSvg className="h-xs-6 w-xs-6 rounded-full" />
+				) : (
+					<img src={avatar} alt="" className="h-xs-6 w-xs-6 rounded-full" />
+				)}
+				<span className="mb-px">{result.role === 'agent' ? 'Lorem Ipsum GPT' : name}</span>
+			</div>
+
+			{/* MESSAGE */}
+			<div className="mt-xs-1 px-button-px-item">
+				<HighlightedText text={result.text} keyword={keyword} className="text-size-sm text-color-text-subtle" />
 			</div>
 		</li>
 	)
