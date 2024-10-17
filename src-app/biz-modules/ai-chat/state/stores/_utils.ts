3954f79deea2f00e5dcc9c8e0aafb5419c6d7374
Morco <hello.morco@gmail.com>
2024-10-17 21:45:49 +0300

    WIP

--- src-app/biz-modules/ai-chat/state/stores/_utils.ts
+++ src-app/biz-modules/ai-chat/state/stores/_utils.ts
@@ -5,6 +5,7 @@ export const GHOST_CHAT = {
 	id: 0,
 	title: 'New chat',
 	createdAt: new Date().toISOString(),
+	size: 0,
 }
 
 export const newGhostMessage = (chatId: number, subchatId: number, role: MessageRole, text: string): Message => ({
